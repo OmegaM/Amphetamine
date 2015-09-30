aa31af1d98333a0375b14b2f6a7b36439054623e
SigmaMiao <sigmamiao@yahoo.com>
2015-09-30 18:57:32 +0800

    修改index.html显示逻辑

--- amphetamine_app/controllers/main_controller.py
+++ amphetamine_app/controllers/main_controller.py
@@ -10,10 +10,9 @@
     main_controller.py
 """
 
-
-
 from amphetamine_app import amphetamine_app, logger, db
 from amphetamine_app.forms.main_form import EditTestCaseForm
+from amphetamine_app.models.mian_model import Amphetamine
 from flask import jsonify, request, render_template, redirect, flash, url_for
 
 
@@ -21,9 +20,65 @@ from flask import jsonify, request, render_template, redirect, flash, url_for
 @amphetamine_app.route('/index')
 def index():
     form = EditTestCaseForm()
-    return render_template('index.html', form=form)
+    amphetamine_list = db.session.query(Amphetamine).order_by(Amphetamine.parent, Amphetamine.id).all()
+    logger.debug('amphetamine_list is : ' + str(amphetamine_list[0]))
+
+    return render_template('index.html', form=form, amphetamine_list=amphetamine_list)
 
 
-@amphetamine_app.route('/add_case')
+@amphetamine_app.route('/add_case', methods=['GET', 'POST'])
 def add_case():
-    pass
+    form = EditTestCaseForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            amphetamine = Amphetamine(page_key=form.page_key.data,
+                                      page_value=form.page_value.data,
+                                      element=form.element.data,
+                                      child=form.child.data,
+                                      child_desc=form.child_desc.data,
+                                      parent=form.parent.data,
+                                      parent_desc=form.parent_desc.data,
+                                      branch=form.branch.data,
+                                      action=form.action.data
+                                      )
+            db.session.add(amphetamine)
+            db.session.commit()
+            flash(u"添加成功")
+            return redirect(url_for('index'))
+        else:
+            flash(u"添加失败")
+            return render_template('index.html', form=form)
+    else:
+        return redirect(url_for('index'))
+
+
+@amphetamine_app.route('/update_case_enable/<int:id>', methods=['GET'])
+def update_case_enable(id):
+
+    if request.method == 'GET':
+        try:
+            case = db.session.query(Amphetamine).get(id)
+            case.is_enable = not case.is_enable
+            db.session.commit()
+            flash(u"修改成功")
+            return redirect(url_for('index'))
+        except Exception, e:
+            logger.debug("update case failed : " + e.message)
+            db.session.rollback()
+    return redirect(url_for('index'))
+
+
+@amphetamine_app.route('/delete_case/<int:id>', methods=['GET'])
+def delete_case(id):
+
+    if request.method == 'GET':
+        try:
+            case = db.session.query(Amphetamine).get(id)
+            db.session.delete(case)
+            db.session.commit()
+            flash(u"删除成功")
+            return redirect(url_for('index'))
+        except Exception, e:
+            logger.debug("delete case failed : " + e.message)
+            db.session.rollback()
+    return redirect(url_for('index'))
---
+++ amphetamine_app/static/js/updateTestCase.js
@@ -0,0 +1,5 @@
+/**
+ * Created by OmegaMiao on 2015/9/30 0030 16:00.
+ */
+
+
