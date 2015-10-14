0fedacacac2d44810be11d29f52eaadab1130bc9
SigmaMiao <sigmamiao@yahoo.com>
2015-10-14 22:11:36 +0800

    优化export excel功能

--- amphetamine_app/controllers/download_controller.py
+++ amphetamine_app/controllers/download_controller.py
@@ -10,7 +10,6 @@
     download_controller.py
 """
 
-
 import xlwt
 import StringIO
 from flask import jsonify, request, make_response
@@ -21,38 +20,45 @@ from ..models.mian_model import Amphetamine as Amp
 
 @amphetamine_app.route('/export_testsuite_xls', methods=['GET'])
 def export_testsuite_xls():
-    wb=xlwt.Workbook()
-    wb.encoding='UTF-8'
-    ws=wb.add_sheet('1')
-    ws.write(0, 0, u'序号')
-    ws.write(0,1, u'用例描述')
-    ws.write(0,2, u'父项')
-    ws.write(0, 3, u'子项')
-    ws.write(0, 4, u'步骤')
-    ws.write(0, 5, u'期望值')
-    sio=StringIO.StringIO()
+
+    sio = StringIO.StringIO()
+
+    workbook = xlwt.Workbook(encoding='utf-8')
+    sheet = workbook.add_sheet('testsuite', cell_overwrite_ok=True)
+    sheet.col(0).width = 1200
+    style = xlwt.XFStyle()
+    font = xlwt.Font()
+    font.name = 'Times'
+    style.font = font
+
+    sheet.write(0, 0, u'序号', style)
+    sheet.write(0, 1, u'用例描述', style)
+    sheet.write(0, 2, u'父项', style)
+    sheet.write(0, 3, u'子项', style)
+    sheet.write(0, 4, u'步骤', style)
+    sheet.write(0, 5, u'期望值', style)
 
     if request.method == 'GET':
         parent = request.args.get('parent')
         child = request.args.get('child')
         try:
             testsuite_list = db.session.query(
-                Amp.element_desc, Amp.parent_desc, Amp.child_desc, Amp.step, Amp.element_value).\
+                Amp.element_desc, Amp.parent_desc, Amp.child_desc, Amp.step, Amp.element_value). \
                 filter(and_(Amp.parent == parent, Amp.child == child)).order_by(Amp.step).all()
             for index, item in enumerate(testsuite_list):
-                # testsuite_dict[index] = item.element_desc
-                ws.write(index +1, 0, index+1)
-                ws.write(index + 1, 1, item.element_desc)
-                ws.write(index + 1, 2, item.parent_desc)
-                ws.write(index + 1, 3, item.child_desc)
-                ws.write(index + 1, 4, item.step)
-                ws.write(index + 1, 5, item.element_value)
+                index += 1
+                sheet.write(index, 0, index, style)
+                sheet.write(index, 1, item.element_desc, style)
+                sheet.write(index, 2, item.parent_desc, style)
+                sheet.write(index, 3, item.child_desc, style)
+                sheet.write(index, 4, item.step, style)
+                sheet.write(index, 5, item.element_value, style)
 
-            wb.save(sio)
+            workbook.save(sio)
             resp = make_response(sio.getvalue())
             resp.headers['Content-type'] = 'application/vnd.ms-excel'
             resp.headers['Transfer-Encoding'] = 'chucked'
-            resp.headers['Content-Disposition'] = 'attachment;filename="export.xls"'
+            resp.headers['Content-Disposition'] = 'attachment;filename="TestSuite.xls"'
             return resp
         except Exception, e:
             logger.error("export excel has an error : " + e.message)
